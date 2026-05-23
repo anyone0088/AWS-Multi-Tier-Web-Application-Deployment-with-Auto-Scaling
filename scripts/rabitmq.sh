a3d178888606b507c17494b8d29052356d51eefa
anyone0088 <deepanshuprabhakar777@gmail.com>
2026-05-23 22:39:21 +0530

    Update rabitmq.sh

--- scripts/rabitmq.sh
+++ scripts/rabitmq.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
-## primary RabbitMQ signing key
-rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc'
-## modern Erlang repository
-rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key'
-## RabbitMQ server repository
-rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key'
-curl -o /etc/yum.repos.d/rabbitmq.repo https://raw.githubusercontent.com/hkhcoder/vprofile-project/refs/heads/awsliftandshift/al2023rmq.repo
-dnf update -y
-## install these dependencies from standard OS repositories
-dnf install socat logrotate -y
-## install RabbitMQ and zero dependency Erlang
-dnf install -y erlang rabbitmq-server
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
-sudo rabbitmqctl add_user test test
-sudo rabbitmqctl set_user_tags test administrator
-rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
 
+# Import RabbitMQ signing keys
+rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/rabbitmq-release-signing-key.asc'
+rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-erlang.E495BB49CC4BBE5B.key'
+rpm --import 'https://github.com/rabbitmq/signing-keys/releases/download/3.0/cloudsmith.rabbitmq-server.9F4587F226208342.key'
+
+# Copy RabbitMQ repository configuration
+sudo cp rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+
+# Update packages
+sudo dnf update -y
+
+# Install dependencies
+sudo dnf install socat logrotate -y
+
+# Install Erlang and RabbitMQ
+sudo dnf install -y erlang rabbitmq-server
+
+# Enable and start service
+sudo systemctl enable rabbitmq-server
+sudo systemctl start rabbitmq-server
+
+# Configure RabbitMQ
+sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
+
+# Create demo user
+sudo rabbitmqctl add_user demo_user demo_password
+sudo rabbitmqctl set_user_tags demo_user administrator
+sudo rabbitmqctl set_permissions -p / demo_user ".*" ".*" ".*"
+
+# Restart service
 sudo systemctl restart rabbitmq-server
